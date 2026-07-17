c64d0fc6dc4d61acbf38d80e1d0a404a675a9645
zakwu <123537200@qq.com>
2026-07-17 08:52:09 +0800

    fix: correct import path in app/page.tsx from ./i18n to ../i18n

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-import { defaultLocale } from "./i18n/config";
+import { defaultLocale } from "../i18n/config";
 
 export default function RootPage() {
   redirect(`/${defaultLocale}`);
