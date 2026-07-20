79e07c40da7d68ca780915792651dccd7fca98b7
zakwu <123537200@qq.com>
2026-07-20 13:26:18 +0800

    fix: set localeDetection: false to prevent Googlebot Accept-Language redirect

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
 import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing";
+import { locales, defaultLocale } from "./i18n/config";
 
-export default createMiddleware(routing);
+export default createMiddleware({
+  locales,
+  defaultLocale,
+  localePrefix: "as-needed",
+  localeDetection: false,
+});
 
 export const config = {
   matcher: [
